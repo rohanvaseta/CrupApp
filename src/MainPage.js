087a40d918420b6f152159e74fb5dac6feb64190
rohan vaseta <rohanvaseta81@gmail.com>
2023-04-10 15:54:13 +0530

    home2

--- src/MainPage.js
+++ src/MainPage.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 
 const MainPage = (props) => {
   return (
-    <div className='overflow'>
+    <div className='overflow2'>
+  
      <Link to="/Form" style={{ textDecoration: 'none' }}><button className={props.isLightModeOn?"button1-light":"button1"} style={{ marginTop:"50px"}}>Add Data</button></Link>
      <Link to="/Table" style={{ textDecoration: 'none' }}><button className={props.isLightModeOn?"button1-light":"button1"} style={{ marginTop:"50px"}}>Show Table</button></Link>
       </div>
+    
   )
 }
 
--- src/TableData.js
+++ src/TableData.js
@@ -5,6 +5,7 @@ const TableData = (props) => {
   return (
     <div>
        <div className={"overflow"}>
+       
       <table className={props.isLightModeOn?"table-light":""}>
             <tr className={props.isLightModeOn?"toprow-light":"toprow"}>
             <td className={props.isLightModeOn?"siderow-light":"siderow"}>Id</td>
@@ -62,7 +63,7 @@ const TableData = (props) => {
         
     
       </table>
-      <div><Link to="/Form" style={{ textDecoration: 'none' }}><button className={props.isLightModeOn?"button1-light":"button1"}>Add More Data</button></Link></div>
+      <div ><Link to="/Form" style={{ textDecoration: 'none' }}><button className={props.isLightModeOn?"button1-light":"button1"}>Add More Data</button></Link></div>
     </div>
     </div>
    
